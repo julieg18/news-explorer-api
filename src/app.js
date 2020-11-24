cac478fff6c2f1232bf817a7c87a54733f3274ab
julieg18 <juliannagal18@gmail.com>
2020-11-24 10:15:36 -0600

    feat: set up central error handler

--- src/app.js
+++ src/app.js
@@ -17,4 +17,12 @@ app.use('/', (req, res) => {
   res.send({ message: 'Requested resource not found' });
 });
 
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'An error occured on the app' : message,
+  });
+});
 module.exports = app;
