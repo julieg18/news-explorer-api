03e6c1d4d6655ee0660ae09b112ad316b91ccb00
julieg18 <juliannagal18@gmail.com>
2020-12-03 06:00:12 -0600

    fix: fix err in middleware/users.js * have app throw validation error if user's password is undefined

--- src/middleware/users.js
+++ src/middleware/users.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const ValidationError = require('../errors/ValidationError');
 
 function validateSignupUserData(req, res, next) {
-  const { name, password, email } = req.body;
+  const { name, password = '', email } = req.body;
   const isPasswordValid = /^\S+$/.test(password) && password.length >= 6;
 
   User.validate({ password, email, name }, ['password', 'email', 'name'])
